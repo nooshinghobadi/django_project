40d7286e8fa246cea919280800e26812377b3600
Nooshin Ghobadi <ghobadinooshin@gmail.com>
2024-05-18 01:48:36 +0330

    improving urls and adding pagination

--- my_blog/firstblog/views.py
+++ my_blog/firstblog/views.py
@@ -1,20 +1,37 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.http import Http404
+from django.core.paginator import Paginator,EmptyPage,\
+                                  PageNotAnInteger
+
 
 def post_list(request):
-    posts=Post.published.all()
+    post_list=Post.published.all()
+    #paigination with 3 posts per page
+    paginator=Paginator(post_list, 3)
+    page_number=request.GET.get('page', 1)
+    try:
+        posts=paginator.page(page_number)
+    except PageNotAnInteger:
+        posts=paginator.page(1)
+    except EmptyPage:
+        #if page_number isnt exist deliver last page
+        posts=paginator.page(paginator.num_pages)
+
     return render(request,
                   'firstblog/post/list.html',
                   {'posts':posts})
-def post_detail(request, id):
+def post_detail(request, year, month, day, post):
     #try:
         #post=Post.published.get(id=id)
     #except Post.DoesNotExist:
         #raise Http404("No post found.")
     post=get_object_or_404(Post,
-                           id=id,
-                           status=Post.Status.published)
+                           status=Post.Status.published,
+                           slug=post,
+                           publish__year=year,
+                           publish__month=month,
+                           publish__day=day)
     
     return render(request,
                   'firstblog/post/detail.html',
--- my_blog/my_blog/settings.py
+++ my_blog/my_blog/settings.py
@@ -117,6 +117,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+#man ezafeh kardam va khata dad:
+#STATICFILES_DIRS = [BASE_DIR / "static"]
+#STATIC_ROOT = BASE_DIR / "assets"
+#print([os.path.join(BASE_DIR, "static")])
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
