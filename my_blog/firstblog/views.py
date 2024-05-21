d11be3ef626fffbd5dd5d5997b4739e05f7ce26c
Nooshin Ghobadi <ghobadinooshin@gmail.com>
2024-05-21 14:40:40 +0330

    add email share and comments feature

--- my_blog/firstblog/views.py
+++ my_blog/firstblog/views.py
@@ -1,26 +1,37 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post
+from .models import Post , Comment
 from django.http import Http404
 from django.core.paginator import Paginator,EmptyPage,\
                                   PageNotAnInteger
+from django.views.generic import ListView
+from .forms import EmailPostForm , CommentForm
+from django.core.mail import send_mail
+from django.views.decorators.http import require_POST
 
 
-def post_list(request):
-    post_list=Post.published.all()
-    #paigination with 3 posts per page
-    paginator=Paginator(post_list, 3)
-    page_number=request.GET.get('page', 1)
-    try:
-        posts=paginator.page(page_number)
-    except PageNotAnInteger:
-        posts=paginator.page(1)
-    except EmptyPage:
-        #if page_number isnt exist deliver last page
-        posts=paginator.page(paginator.num_pages)
+#def post_list(request):
+    #post_list=Post.published.all()
+    ##paigination with 3 posts per page
+    #paginator=Paginator(post_list, 3)
+    #page_number=request.GET.get('page', 1)
+    #try:
+        #posts=paginator.page(page_number)
+    #except PageNotAnInteger:
+        #posts=paginator.page(1)
+    #except EmptyPage:
+        ##if page_number isnt exist deliver last page
+        #posts=paginator.page(paginator.num_pages)
 
-    return render(request,
-                  'firstblog/post/list.html',
-                  {'posts':posts})
+    #return render(request,
+                  #'firstblog/post/list.html',
+                  #{'posts':posts})
+
+class PostListView(ListView):
+    queryset=Post.published.all()
+    context_object_name='posts'
+    paginate_by=3
+    template_name='firstblog/post/list.html'
+
 def post_detail(request, year, month, day, post):
     #try:
         #post=Post.published.get(id=id)
@@ -33,7 +44,47 @@ def post_detail(request, year, month, day, post):
                            publish__month=month,
                            publish__day=day)
     
+    comments=post.comments.filter(active=True)
+    form=CommentForm()
+
     return render(request,
                   'firstblog/post/detail.html',
-                  {'post':post})
+                  {'post':post,
+                   'comments':comments,
+                   'form':form})
 
+def post_share(request, post_id):
+    post=get_object_or_404(Post, id=post_id, status=Post.Status.published)
+    sent = False
+
+    if request.method == 'POST':
+        form=EmailPostForm(request.POST)
+        if form.is_valid():
+            cd=form.cleaned_data
+            post_url = request.build_absolute_uri(
+                       post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read "\
+                      f"{post.title}"
+            message = f"Read {post.title} at {post_url}\n\n"\
+                      f"{cd['name']}\'s comments: {cd['comments']}"
+            send_mail(subject, message, 'nooshinghobadi0@gmail.com',
+                      [cd['to']])
+            sent = True
+    else:
+        form=EmailPostForm()
+    return render(request, 'firstblog/post/share.html',
+                            {'post':post,'form':form,'sent': sent})
+
+@require_POST
+def post_comment(request, post_id):
+    post=get_object_or_404(Post, id=post_id, status=Post.Status.published)
+    comment=None
+    form=CommentForm(data=request.POST)
+    if form.is_valid():
+        comment=form.save(commit=False)
+        comment.post=post
+        comment.save()
+    return render(request, 'firstblog/post/comment.html',
+                  {'post':post,
+                  'form':form,
+                  'comment':comment})
--- my_blog/my_blog/settings.py
+++ my_blog/my_blog/settings.py
@@ -126,3 +126,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'nooshinghobadi0@gmail.com'
+EMAIL_HOST_PASSWORD = 'qizlcczhhdwbwbfm'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
