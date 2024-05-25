2791140a113fd5f130d7981d9cdd136af0fbcb61
Nooshin Ghobadi <ghobadinooshin@gmail.com>
2024-05-25 04:20:28 +0330

    feed

--- my_blog/firstblog/urls.py
+++ my_blog/firstblog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from .feeds import LastestPostFeed
 
 app_name="firstblog"
 
@@ -14,4 +15,6 @@ urlpatterns=[
          views.post_share, name='post_share'),
     path('<int:post_id>/comment/',
          views.post_comment, name='post_comment'),
+     
+    path('feed/', LastestPostFeed(), name='post_feed'),
 ]
