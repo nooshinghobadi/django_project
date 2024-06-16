5c6aeb06c98abb3a4628a26a27cc8d8117237078
Nooshin Ghobadi <ghobadinooshin@gmail.com>
2024-06-17 01:43:25 +0330

    asynchronous tasks(with celery,rabbitmq and flower)

--- my_blog/orders/tasks.py
+++ my_blog/orders/tasks.py
@@ -5,10 +5,7 @@ from .models import Order
 
 @shared_task
 def order_created(order_id):
-    """
-    Task to send an e-mail notification when an order is
-    successfully created.
-    """
+    
     order = Order.objects.get(id=order_id)
     subject = f'Order nr. {order.id}'
     message = f'Dear {order.first_name},\n\n' \
