9c19df50796aaa43fd9b6edb15b10ad884a80273
Nooshin Ghobadi <ghobadinooshin@gmail.com>
2024-06-29 16:45:32 +0330

    need to complete

---
+++ my_blog/my_blog/settings/prod.py
@@ -0,0 +1,20 @@
+from .base import *
+import os
+DEBUG = False
+
+ADMIN = [
+    ('Nooshin', 'nooshinghobadi0@gmail.com'),
+]
+
+ALLOWED_HOSTS = ['*']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DB'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': 'db',
+        'PORT': 5432,
+    }
+}
